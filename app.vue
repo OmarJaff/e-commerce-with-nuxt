e9bafcf19e7f6b3cd045be92a0ef08b19fbafdf3
Omar Jaff <me@omarjaff.com>
2022-12-30 18:49:08 +0300

    Created the structure of the project

--- app.vue
+++ app.vue
@@ -1,11 +1,28 @@
 <template>
-   <Header>
 
-   </Header>
-   <ProductList>
-    
-   </ProductList>
-   <Footer>
+	<Header>
+		<template v-slot:nav-item>
+			<NuxtLink class="font-semibold text-gray-900 hover:text-gray-900" to="/"
+				>Home</NuxtLink
+			>
+			<NuxtLink
+				class="font-semibold text-gray-900 hover:text-gray-900"
+				to="/products"
+				>Products</NuxtLink
+			>
+			<NuxtLink
+				class="font-semibold text-gray-900 hover:text-gray-900"
+				to="/contact"
+				>Contact Us</NuxtLink
+			>
+		</template>
+	</Header>
+	
+    <NuxtPage>
+      
+    </NuxtPage>
 
-   </Footer>
+	<Footer> 
+        
+    </Footer>
 </template>
--- components/Header.vue
+++ components/Header.vue
@@ -31,13 +31,10 @@
           </button>
         </div>
         <div class="hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-center lg:gap-x-12">
-          <a href="#" class="font-semibold text-gray-900 hover:text-gray-900">Product</a>
-
-          <a href="#" class="font-semibold text-gray-900 hover:text-gray-900">Features</a>
-
-          <a href="#" class="font-semibold text-gray-900 hover:text-gray-900">Marketplace</a>
-
-          <a href="#" class="font-semibold text-gray-900 hover:text-gray-900">Company</a>
+            <slot name="nav-item">
+              
+            </slot>
+    
         </div>
         <div class="hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-end">
           <a href="#" class="inline-block rounded-lg px-3 py-1.5 text-sm font-semibold leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20">Log in</a>
