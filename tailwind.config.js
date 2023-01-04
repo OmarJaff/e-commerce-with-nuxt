aad33ab12eaa5d38a24144d43f28d9b1a8833783
Omar Jaff <me@omarjaff.com>
2023-01-04 21:44:46 +0300

    Fixed a warning message

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,9 @@
 module.exports = {
   
   content: [
-    "./assets/**/*.{css}",
     "./components/*.{vue,js}",
     "./components/**/*.{vue,js}",
     "./pages/*.vue",
-    "./pages/**/*.vue",
     "./plugins/**/*.{js,ts}",
     "./*.{vue,js,ts}",
     "./nuxt.config.{js,ts}",
