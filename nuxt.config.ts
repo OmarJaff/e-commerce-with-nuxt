17bd7146bc26ef43ab1ac3387d4208f5faa8570c
Omar Jaff <me@omarjaff.com>
2023-01-04 21:04:34 +0300

    Added Strapi plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,16 @@ export default defineNuxtConfig({
      ],
     modules: [
         '@pinia/nuxt',
+        '@nuxtjs/strapi'
       ],
+    strapi: {
+        url: process.env.STRAPI_URL || 'http://localhost:1337',
+        prefix: '/api',
+        version: 'v4',
+        cookie: {},
+        cookieName: 'strapi_jwt',
+       
+      }
     
     
 })
